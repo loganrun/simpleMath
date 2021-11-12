655973481acffb41d352e515af1c5c7ce70984b1
loganrun <bigdog_41@hotmai.com>
2021-11-12 00:34:35 -0800

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+
 import './App.css';
+import MathQuest from '../src/components/mathQuest'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="box">
+        <p>Instructions</p>
+        <MathQuest/>
+
+      </div>
+      <div className="box">
+        <p>Fill it in here</p>
+        <MathQuest/>
+
+      </div>
+      
     </div>
   );
 }
---
+++ src/components/mathQuest.js
@@ -0,0 +1,18 @@
+import React, { useEffect, useState } from 'react'
+import './mathQuest.css'
+
+
+
+const  MathQuest = (props) => {
+const [addA ,setaddA] = useState(null)
+
+
+
+    return (
+        <div className={"box1"}>
+            <p>98 + 57</p>
+        </div>
+    );
+}
+
+export default MathQuest;
